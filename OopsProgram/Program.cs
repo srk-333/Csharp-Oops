bf093a2a91d35d221794b7fc4254d4b9f72ad383
srk-333 <sauravmk333@gmail.com>
2022-01-11 12:34:35 +0530

    [saurav] Added Welcome program

---
+++ OopsProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to OOPs program Practice");
+            Console.ReadLine();
+        }
+    }
+}
